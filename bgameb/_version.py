4a3ba325f1b87df915c67627181578b131115f81
Konstantin Klepikov <oformleno@gmail.com>
2023-01-14 00:48:06 +0200

    release 0.0.39

--- bgameb/_version.py
+++ bgameb/_version.py
@@ -7,5 +7,5 @@ Provides bgameb version information.
 
 from incremental import Version
 
-__version__ = Version("bgameb", 0, 0, 38)
+__version__ = Version("bgameb", 0, 0, 39)
 __all__ = ["__version__"]
